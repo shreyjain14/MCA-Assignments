b93a11c43794fffeca1aead44057463daa6a79fc
Shrey <shreyjain1403@gmail.com>
2024-08-03 08:38:23 +0530

    updated excerise 3 to b e menu based

--- Trimester-1/Python/Lab-Excerise-3/main.py
+++ Trimester-1/Python/Lab-Excerise-3/main.py
@@ -2,6 +2,16 @@ import smartscan_registration_module as sr
 
 if __name__ == '__main__':
 
-    sr.RegisterUserFromSmartScan('Shrey,shrey@shreyjain.me')
-    sr.RegisterUserFromSmartScan('Anjaney,mitraanjaney@gmail.com')
-    sr.RegisterUserFromSmartScan('Dave,davesharma0002@gmail.com')
+    hardcoded = False
+
+    if hardcoded:
+        sr.RegisterUserFromSmartScan('Shrey,shrey@shreyjain.me')
+        sr.RegisterUserFromSmartScan('Anjaney,mitraanjaney@gmail.com')
+        sr.RegisterUserFromSmartScan('Dave,davesharma0002@gmail.com')
+
+    else:
+        while True:
+            data = input('Enter scanned data [q to exit]: ')
+            if data == 'q':
+                break
+            sr.RegisterUserFromSmartScan(data)
