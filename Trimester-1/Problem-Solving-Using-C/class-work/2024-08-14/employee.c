da6aeb3e25f97c0b6c14f702df3118c4e11d237a
Shrey <shreyjain1403@gmail.com>
2024-08-14 11:12:48 +0530

    c classwork added

---
+++ Trimester-1/Problem-Solving-Using-C/class-work/2024-08-14/employee.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+struct employee
+{
+    char name[50];
+    int id;
+    float salary;
+};
+
+int main()
+{
+    struct employee emp1;
+    struct employee emp2;
+
+    strcpy(emp1.name, "John");
+    emp1.id = 1;
+    emp1.salary = 10000;
+
+    strcpy(emp2.name, "Alex");
+    emp2.id = 2;
+    emp2.salary = 20000;
+
+    printf("Employee 1\n");
+    printf("Name: %s\n", emp1.name);
+    printf("ID: %d\n", emp1.id);
+    printf("Salary: %.2f\n", emp1.salary);
+
+    printf("\nEmployee 2\n");
+    printf("Name: %s\n", emp2.name);
+    printf("ID: %d\n", emp2.id);
+    printf("Salary: %.2f\n", emp2.salary);
+
+    return 0;
+}
