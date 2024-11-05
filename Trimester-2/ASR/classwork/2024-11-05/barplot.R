106b280b61b28370a0eba46a03e5b1a848262959
Shrey <shreyjain1403@gmail.com>
2024-11-05 08:02:32 +0530

    added classwork of barplot

---
+++ Trimester-2/ASR/classwork/2024-11-05/barplot.R
@@ -0,0 +1,5 @@
+help("barplot")
+q = c(1,12,12,1,12,2,3,1,2,1,1,1,12)
+tq = table(q)/length(q)
+tq
+barplot(tq, main="something something", names.arg=c('a', 'b', 'c', 'd'), col=c("red", "blue", "yellow", "pink"), ylab="Rel Freq", xlab = "Classes")
