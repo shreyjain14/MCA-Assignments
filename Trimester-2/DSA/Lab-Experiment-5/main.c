2d5cd9cc078b979a81d0dc9b4b2b03b58cefcb9e
Shrey <shreyjain1403@gmail.com>
2024-11-07 18:32:21 +0530

    updated lab 5

--- Trimester-2/DSA/Lab-Experiment-5/main.c
+++ Trimester-2/DSA/Lab-Experiment-5/main.c
@@ -5,19 +5,21 @@
 
 int main()
 {
-
-    int productID[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int productID[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    float prices[10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
 
     while (1)
     {
-
         printf("\n=================================================\n");
         printf("1. Binary Search ProductIDs\n");
         printf("2. Linear Search ProductIDs\n");
         printf("3. Sentinel Search ProductIDs\n");
-        printf("4. Bubble Sort ProsuctIDs\n");
-        printf("5. Insertion Sort ProsuctIDs\n");
-        printf("6. Exit\n");
+        printf("4. Binary Search Prices\n");
+        printf("5. Linear Search Prices\n");
+        printf("6. Sentinel Search Prices\n");
+        printf("7. Bubble Sort ProductIDs\n");
+        printf("8. Insertion Sort ProductIDs\n");
+        printf("9. Exit\n");
         printf("=================================================\n");
 
         int choice;
@@ -33,25 +35,25 @@ int main()
             int index = binarySearch(productID, 0, 9, key);
             if (index == -1)
             {
-                printf("productID not found\n");
+                printf("ProductID not found\n");
             }
             else
             {
-                printf("productID found at index %d\n", index);
+                printf("ProductID found at index %d\n", index);
             }
             break;
         case 2:
             printf("Enter the productID to be searched: ");
             int key2;
             scanf("%d", &key2);
-            int index2 = linerSearch(productID, 10, key2);
+            int index2 = linearSearch(productID, 10, key2);
             if (index2 == -1)
             {
-                printf("productID not found\n");
+                printf("ProductID not found\n");
             }
             else
             {
-                printf("productID found at index %d\n", index2);
+                printf("ProductID found at index %d\n", index2);
             }
             break;
         case 3:
@@ -61,40 +63,84 @@ int main()
             int index3 = sentinelSearch(productID, 10, key3);
             if (index3 == -1)
             {
-                printf("productID not found and not enough space to insert\n");
+                printf("ProductID not found and not enough space to insert\n");
             }
             else
             {
-                printf("productID found at index %d\n", index3);
+                printf("ProductID found at index %d\n", index3);
             }
             break;
         case 4:
-            printf("Enter 10 productIDs to be sorted: ");
-            int arr[10];
-            for (int i = 0; i < 10; i++)
+            printf("Enter the price to be searched: ");
+            float priceKey;
+            scanf("%f", &priceKey);
+            int priceIndex = binarySearchFloat(prices, 0, 9, priceKey);
+            if (priceIndex == -1)
             {
-                scanf("%d", &arr[i]);
+                printf("Price not found\n");
             }
-            bubbleSort(arr, 10);
-            for (int i = 0; i < 10; i++)
+            else
             {
-                printf("%d ", arr[i]);
+                printf("Price found at index %d\n", priceIndex);
             }
             break;
         case 5:
+            printf("Enter the price to be searched: ");
+            float priceKey2;
+            scanf("%f", &priceKey2);
+            int priceIndex2 = linearSearchFloat(prices, 10, priceKey2);
+            if (priceIndex2 == -1)
+            {
+                printf("Price not found\n");
+            }
+            else
+            {
+                printf("Price found at index %d\n", priceIndex2);
+            }
+            break;
+        case 6:
+            printf("Enter the price to be searched: ");
+            float priceKey3;
+            scanf("%f", &priceKey3);
+            int priceIndex3 = sentinelSearchFloat(prices, 10, priceKey3);
+            if (priceIndex3 == -1)
+            {
+                printf("Price not found and not enough space to insert\n");
+            }
+            else
+            {
+                printf("Price found at index %d\n", priceIndex3);
+            }
+            break;
+        case 7:
             printf("Enter 10 productIDs to be sorted: ");
             int arr2[10];
             for (int i = 0; i < 10; i++)
             {
                 scanf("%d", &arr2[i]);
             }
-            insertionSort(arr2, 10);
+            bubbleSort(arr2, 10);
             for (int i = 0; i < 10; i++)
             {
                 printf("%d ", arr2[i]);
             }
+            printf("\n");
             break;
-        case 6:
+        case 8:
+            printf("Enter 10 productIDs to be sorted: ");
+            int arr[10];
+            for (int i = 0; i < 10; i++)
+            {
+                scanf("%d", &arr[i]);
+            }
+            insertionSort(arr, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
+            break;
+        case 9:
             exit(0);
             break;
         default:
--- Trimester-2/DSA/Lab-Experiment-5/searching.h
+++ Trimester-2/DSA/Lab-Experiment-5/searching.h
@@ -23,10 +23,9 @@ int binarySearch(int *arr, int min, int max, int key)
     return -1;
 }
 
-int linerSearch(int *arr, int n, int key)
+int linearSearch(int *arr, int n, int key)
 {
-    int i;
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] == key)
         {
@@ -38,10 +37,9 @@ int linerSearch(int *arr, int n, int key)
 
 int sentinelSearch(int *arr, int n, int key)
 {
-    int i;
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (key == arr[i])
+        if (arr[i] == key)
         {
             return i;
         }
@@ -51,8 +49,57 @@ int sentinelSearch(int *arr, int n, int key)
         arr[n] = key;
         return n;
     }
-    else
+    return -1;
+}
+
+// Functions for searching floats
+int binarySearchFloat(float *arr, int min, int max, float key)
+{
+    int mid;
+    while (min <= max)
     {
-        return -1;
+        mid = (min + max) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            max = mid - 1;
+        }
+        else
+        {
+            min = mid + 1;
+        }
     }
+    return -1;
+}
+
+int linearSearchFloat(float *arr, int n, float key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int sentinelSearchFloat(float *arr, int n, float key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i;
+        }
+    }
+    if (sizeof(arr) / sizeof(arr[0]) > n)
+    {
+        arr[n] = key;
+        return n;
+    }
+    return -1;
 }
