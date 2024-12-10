7205b8da0cd3ffe3e6dcdbfac6d97f51c00ab300
shreyjain14 <shreyjain1403@gmail.com>
2024-12-10 12:25:26 +0530

    updated tax bracket

--- Trimester-2/SE/JavaTesting/src/main/java/com/shreyjain/tax/TaxSlabs.java
+++ Trimester-2/SE/JavaTesting/src/main/java/com/shreyjain/tax/TaxSlabs.java
@@ -10,42 +10,42 @@ public class TaxSlabs {
             return -1;
         }
         
-        if (age > 60) {
-            if (income < 250000) {
+        if (age < 60) {
+            if (income <= 250000) {
                 return 0;
-            } else if (250001 < income && income < 500000) {
+            } else if (250001 <= income && income <= 500000) {
                 return 5;
-            } else if (500001 < income && income < 750000) {
+            } else if (500001 <= income && income <= 750000) {
                 return 10;
-            } else if (750001 < income && income < 1000000) {
+            } else if (750001 <= income && income <= 1000000) {
                 return 15;
-            } else if (1000001 < income && income < 1250000) {
+            } else if (1000001 <= income && income <= 1250000) {
                 return 20;
-            } else if (1250001 < income && income < 1500000) {
+            } else if (1250001 <= income && income <= 1500000) {
                 return 25;
             } else {
                 return 30;
             }
-        } else if (age >= 60 && age < 80) {
-            if (income < 300000) {
+        } else if (60 <= age && age < 80) {
+            if (income <= 300000) {
                 return 0;
-            } else if (300001 < income && income < 500000) {
+            } else if (300001 <= income && income <= 500000) {
                 return 5;
-            } else if (500001 < income && income < 750000) {
+            } else if (500001 <= income && income <= 750000) {
                 return 10;
-            } else if (750001 < income && income < 1000000) {
+            } else if (750001 <= income && income <= 1000000) {
                 return 15;
-            } else if (1000001 < income && income < 1250000) {
+            } else if (1000001 <= income && income <= 1250000) {
                 return 20;
-            } else if (1250001 < income && income < 1500000) {
+            } else if (1250001 <= income && income <= 1500000) {
                 return 25;
             } else {
                 return 30;
             }
         } else {
-            if (income < 500000) {
+            if (income <= 500000) {
                 return 0;
-            } else if (500001 < income && income < 1000000) {
+            } else if (500001 <= income && income <= 1000000) {
                 return 20;
             } else {
                 return 30;
