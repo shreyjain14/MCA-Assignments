bb2fcc11503f3e3776f0a663371f8e626b93a0d2
shreyjain14 <shreyjain1403@gmail.com>
2025-01-23 10:43:40 +0530

    updated hello world file

--- Trimester-3/GoLang/classroom/2025-01-23/helloworld.go
+++ Trimester-3/GoLang/classroom/2025-01-23/helloworld.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	dmt.Println "Hello World!"
+	// fmt.Println("Hello World!")
+
+	// const age, name = 20, "John"
+	// const age1, age2 = 20, 30
+
+	// const avgAge = (age1 + age2) / 2
+
+	// fmt.Println(avgAge)
+
+	// fmt.Println("Age: ", age, "Name: ", name)
+	// fmt.Printf("Age: %v Name: %v\n", age, name)
+	// fmt.Printf("Age: %d Name: %s\n", age, name)
+
+	// msg := fmt.Sprintf("Age: %d Name: %s\n", age, name)
+	// fmt.Println(msg)
+
+	var age3 float32
+	var name1 string
+
+	// fmt.Print("Enter your age: ")
+	// fmt.Scan(&age3)
+
+	// fmt.Print("Enter your name: ")
+	// fmt.Scan(&name1)
+
+	fmt.Printf("Age: %f Name: %s\n", age3, name1)
 }
